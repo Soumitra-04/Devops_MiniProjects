2c4fa953945c03d07147fb74fee789b0da293f39
Soumitra-04 <soumitrarajguru1@gmail.com>
2026-06-09 23:36:05 +0530

    initial commit : added user management system

---
+++ app.py
@@ -0,0 +1,25 @@
+users = []
+
+def show_menu():
+    print("\n=== User Management System ===")
+    print("1. View Users")
+    print("2. Exit")
+
+while True:
+    show_menu()
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        if not users:
+            print("No users found.")
+        else:
+            for user in users:
+                print(user)
+
+    elif choice == "2":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice.")
